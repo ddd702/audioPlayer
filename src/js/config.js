8f3efa310eae22a727b96ece1ef043fabb076b92
邓的灯 <2008.ddd@163.com>
2017-09-19 22:19:39 +0800

    add song

--- src/js/config.js
+++ src/js/config.js
@@ -1,69 +1,85 @@
-var Config=[{
-	pic:'//res.70read.com/images/0e25ccdc279d3f8abedffcf7baa6d372.jpg?imageView2/1/w/200/h/200',
-	name:'ハタ モトヒロ-Rain',
-	url:'//res.70read.com/Rain.mp3'
-},{
-	pic:'http://p4.music.126.net/SVJNKYtlB_n9I7p0017Ndw==/6635552674995921.jpg?param=130y130',
-	name:'TV动画《寄生兽 生命的准则》配乐-NEXT TO YOU',
-	url:'//res.70read.com/nexttoyou.mp3'
-},{
-	pic:'http://p3.music.126.net/cUTk0ewrQtYGP2YpPZoUng==/3265549553028224.jpg?param=130y130',
-	name:'周杰伦-告白气球',
-	url:'//res.70read.com/gaobaiqiqiu.mp3'
-},{
-	name:'Dido - This Land Is Mine',
-	pic:'//musicdata.baidu.com/data2/pic/117822544/117822544.jpg',
-	url:'//res.70read.com/41574312.mp3'
-},{
-	name:'五月天-干杯',
-	pic:'//img.xiami.net/images/album/img10/3110/700711324010597_2.jpg',
-	url:'//res.70read.com/ganbei.mp3'
-},{
-	name:'陈奕迅-陪你度过漫长岁月',
-	pic:"//p3.music.126.net/qW4N08_Q8PSePV7iewwvHg==/3438172860758204.jpg?param=130y130",
-	url:'//res.70read.com/peiniduguo.mp3'
-},{
-	name:'东山少爷-长堤1973',
-	pic:'//p4.music.126.net/lBdgotve2M3RiiMBnXAYjw==/114349209301877.jpg?param=130y130',
-	url:'//res.70read.com/changdi.mp3'
-},{
-	name:'Vox Angeli-New Soul',
-	pic:'//p3.music.126.net/ITgUWlDAPxVTczeaYPY7tQ==/6663040465347914.jpg?param=130y130',
-	url:'//res.70read.com/newsoul.mp3'
-},{
-	pic:'//p3.music.126.net/ffDWh-Nxup64dKJxlvPOrw==/5956054487877602.jpg',
-	name:'Hailey Rowe-My Boyfriend Is Gay',
-	url:'//res.70read.com/MyBoyfriendIsGay.mp3'
-},{
-	pic:'//p4.music.126.net/UMhpY8l46jFatops_6cJUA==/6670737045790100.jpg?param=130y130',
-	name:'古巨基-爱得太迟',
-	url:'//res.70read.com/aidetaichi.mp3'
-},{
-	pic:'//p3.music.126.net/TKIlLLiEmIonBvl3fOEW_A==/94558000006014.jpg?param=130y130',
-	name:'郑中基-无赖',
-	url:'//res.70read.com/wulai.mp3'
-},{
-	pic:'//p3.music.126.net/gGJZ-zgThqL9aYRUhNY0KA==/41781441867562.jpg?param=130y130',
-	name:'萧敬腾-新不了情',
-	url:'//res.70read.com/xinbuleqing.mp3'
-},{
-	pic:'//p3.music.126.net/wQieYFAByMNU5WTXRQtCSg==/1396379780245779.jpg',
-	name:'邓紫棋-夜空中最亮的星',
-	url:'//res.70read.com/zuiliang.mp3'
-},{
-	pic:'//p4.music.126.net/UTrFxVAoq-Qh50dB_M74dw==/51677046522530.jpg',
-	name:'贝多芬-月光奏鸣曲',
-	url:'//res.70read.com/beiduofenyueguang.mp3'
-},{
-	pic:'//p3.music.126.net/pk-xo-6Nu5bwrA-ylVJFUA==/17885755649212510.jpg',
-	name:'夏夜-四季音色',
-	url:'//res.70read.com/xiaye.mp3'
-},{
-	pic:'//p3.music.126.net/8qHI7sEEk3eM2DLitQXFDQ==/765260092953631.jpg?param=130y130',
-	name:'Lily Allen-Fuck You',
-	url:'//res.70read.com/fuckyou.mp3'
-},{
-	pic:'http://img.xiami.net/images/album/img10/3110/3059791379063569_2.jpg',
-	name:'五月天-你不是真正的快乐',
-	url:'//res.70read.com/notrealyhappy.mp3'
+var Config = [{
+    pic: '//p1.music.126.net/fgQF-1te8sQFTpCgLs9oVw==/53876069778057.jpg?param=130y130',
+    name: 'Today-梁咏琪',
+    url: '//res.70read.com/music/Today.mp3'
+}, {
+    pic: '//p1.music.126.net/hNz-BNHDmaF_YsBxk0qxXw==/6647647301587569.jpg?param=200y200',
+    name: '男子组',
+    url: '//res.70read.com/music/nanzizu.mp3'
+}, {
+    pic: 'http://p1.music.126.net/XaLeBWRiNr1qCkH878s_mA==/2942293116305429.jpg?param=130y130',
+    name: '你好可爱-吴克群&宋智孝',
+    url: '//res.70read.com/music/nihaokeai.mp3'
+}, {
+    pic: 'http://p1.music.126.net/W0PO47SHyrVFxYv8DDLCmQ==/18762066418064065.jpg?param=130y130',
+    name: '粤语残片-陈奕迅',
+    url: '//res.70read.com/music/yueyucanpian.mp3'
+}, {
+    pic: 'http://p1.music.126.net/tV4ABEt9ZIF_bn6bvHGnMA==/5925268162301667.jpg?param=130y130',
+    name: 'Good Day-Lucky Stroke',
+    url: '//res.70read.com/music/goodday.mp3'
+}, {
+    pic: 'http://p1.music.126.net/daZcHVIJicL3wXJWMIjAng==/7926379325753633.jpg?param=130y130',
+    name: 'secret base ~君がくれたもの~ (未闻花名片尾曲)',
+    url: '//res.70read.com/music/weiwenhuaming.mp3'
+}, {
+    pic: '//res.70read.com/images/0e25ccdc279d3f8abedffcf7baa6d372.jpg?imageView2/1/w/200/h/200',
+    name: 'Rain-ハタ モトヒロ',
+    url: '//res.70read.com/Rain.mp3'
+}, {
+    pic: 'http://p4.music.126.net/SVJNKYtlB_n9I7p0017Ndw==/6635552674995921.jpg?param=130y130',
+    name: 'NEXT TO YOU-TV动画《寄生兽 生命的准则》配乐',
+    url: '//res.70read.com/nexttoyou.mp3'
+}, {
+    pic: 'http://p3.music.126.net/cUTk0ewrQtYGP2YpPZoUng==/3265549553028224.jpg?param=130y130',
+    name: '告白气球-周杰伦',
+    url: '//res.70read.com/gaobaiqiqiu.mp3'
+}, {
+    name: 'This Land Is Mine-Dido',
+    pic: '//musicdata.baidu.com/data2/pic/117822544/117822544.jpg',
+    url: '//res.70read.com/41574312.mp3'
+}, {
+    name: '干杯-五月天',
+    pic: '//img.xiami.net/images/album/img10/3110/700711324010597_2.jpg',
+    url: '//res.70read.com/ganbei.mp3'
+}, {
+    name: '陪你度过漫长岁月-陈奕迅',
+    pic: "//p3.music.126.net/qW4N08_Q8PSePV7iewwvHg==/3438172860758204.jpg?param=130y130",
+    url: '//res.70read.com/peiniduguo.mp3'
+}, {
+    name: '长堤1973-东山少爷',
+    pic: '//p4.music.126.net/lBdgotve2M3RiiMBnXAYjw==/114349209301877.jpg?param=130y130',
+    url: '//res.70read.com/changdi.mp3'
+}, {
+    name: 'New Soul-Vox Angeli',
+    pic: '//p3.music.126.net/ITgUWlDAPxVTczeaYPY7tQ==/6663040465347914.jpg?param=130y130',
+    url: '//res.70read.com/newsoul.mp3'
+}, {
+    pic: '//p3.music.126.net/ffDWh-Nxup64dKJxlvPOrw==/5956054487877602.jpg',
+    name: 'My Boyfriend Is Gay-Hailey Rowe',
+    url: '//res.70read.com/MyBoyfriendIsGay.mp3'
+}, {
+    pic: '//p4.music.126.net/UMhpY8l46jFatops_6cJUA==/6670737045790100.jpg?param=130y130',
+    name: '爱得太迟-古巨基',
+    url: '//res.70read.com/aidetaichi.mp3'
+}, {
+    pic: '//p3.music.126.net/TKIlLLiEmIonBvl3fOEW_A==/94558000006014.jpg?param=130y130',
+    name: '无赖-郑中基',
+    url: '//res.70read.com/wulai.mp3'
+}, {
+    pic: '//p3.music.126.net/gGJZ-zgThqL9aYRUhNY0KA==/41781441867562.jpg?param=130y130',
+    name: '新不了情-萧敬腾',
+    url: '//res.70read.com/xinbuleqing.mp3'
+}, {
+    pic: '//p4.music.126.net/UTrFxVAoq-Qh50dB_M74dw==/51677046522530.jpg',
+    name: '月光奏鸣曲-贝多芬',
+    url: '//res.70read.com/beiduofenyueguang.mp3'
+}, {
+    pic: '//p3.music.126.net/8qHI7sEEk3eM2DLitQXFDQ==/765260092953631.jpg?param=130y130',
+    name: 'Fuck You-Lily Allen',
+    url: '//res.70read.com/fuckyou.mp3'
+}, {
+    pic: 'http://img.xiami.net/images/album/img10/3110/3059791379063569_2.jpg',
+    name: '你不是真正的快乐-五月天',
+    url: '//res.70read.com/notrealyhappy.mp3'
 }]
