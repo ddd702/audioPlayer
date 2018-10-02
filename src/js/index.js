4497bb97e0b1b1f6f0d2ff135fd6ecb2d8c83470
邓的灯 <2008.ddd@163.com>
2018-10-02 13:37:39 +0800

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import Sound from './sound';
-var $=require('jquery');
+var $ = require('jquery');
 require('dialog.d');
 require('./jquery.slider');
 var el_audio = document.querySelector('#j-audio');
@@ -12,98 +12,108 @@ el_canvas.height = document.querySelector('html').clientHeight;
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame;
 window.cancelAnimationFrame = window.cancelAnimationFrame || window.webkitCancelAnimationFrame || window.mozCancelAnimationFrame || window.msCancelAnimationFrame;
-window.media = new Sound(el_audio,el_canvas);
+window.media = new Sound(el_audio, el_canvas);
 
-class app{
-    constructor(btns,m) {
-        this.listTpl=`<&data.forEach(function(el,index){&><li class="list-itm <&if(index===0){&>active<&}&>" data-index='<&=index&>'>
+class app {
+    constructor(btns, m) {
+        this.listTpl = `<&data.forEach(function(el,index){&><li class="list-itm <&if(index===0){&>active<&}&>" data-index='<&=index&>'>
         <&=el.name&></li><&});&>`;
-        this.btns=btns;
-        this.m=m;
-        this.musicList=Config;
-        this.musicIndex=0;
-        this.$face=$('#avatar');
-        this.$title=$('#title');
+        this.btns = btns;
+        this.m = m;
+        this.musicList = Config;
+        this.musicIndex = 0;
+        this.$face = $('#avatar');
+        this.$title = $('#title');
         this.faceTitle(0);
         this.listMusic();
         this.setEvents();
-        this.m.a.src=this.musicList[0].url;
+        this.m.a.src = this.musicList[0].url;
         console.warn(this.musicList);
     }
-    listMusic(){
-        var _this=this;
-        var htmlStr=D.utils.render(_this.listTpl,{data:_this.musicList});
+    listMusic() {
+        var _this = this;
+        var htmlStr = D.utils.render(_this.listTpl, { data: _this.musicList });
         $('#list').html(htmlStr);
     }
-    set listIndex(index){
-        this.musicIndex=index;
-        this.m.link=this.musicList[index].url;
+    set listIndex(index) {
+        this.musicIndex = index;
+        this.m.link = this.musicList[index].url;
         this.faceTitle(index);
         this.$listItm.removeClass('active').eq(index).addClass('active');
     }
-    faceTitle(index){
-        this.$face.attr('src',this.musicList[index].pic);
+    faceTitle(index) {
+        this.$face.attr('src', this.musicList[index].pic);
         this.$title.html(this.musicList[index].name);
     }
-    afterEnd(){
+    afterEnd() {
         this.btns.play.removeClass('icon-zanting');
         this.$face.removeClass('rotating');
     }
-    afterStart(){
+    afterStart() {
         this.btns.play.addClass('icon-zanting');
         this.$face.addClass('rotating');
     }
-    setEvents(){
+    setEvents() {
         console.info(this.musicList);
-        var _this=this;
-        var $progress=$('#progress');
-        var $nowTime=$('#nowTime');
-        var $allTime=$('#allTime');
-        this.$listItm=$('.list-itm');
-        $("#slider1").slideBar({max:100,min:0,crossC:"#f70",handlerC:'#fff',defalutNum:100,callBack:function(v){
-            media.volumn=parseInt(v)/100;
-        }});
-        this.m.endCb=function(){
+        var _this = this;
+        var $progress = $('#progress');
+        var $nowTime = $('#nowTime');
+        var $allTime = $('#allTime');
+        this.$listItm = $('.list-itm');
+        $("#slider1").slideBar({
+            max: 100,
+            min: 0,
+            crossC: "#f70",
+            handlerC: '#fff',
+            defalutNum: 100,
+            callBack: function(v) {
+                media.volumn = parseInt(v) / 100;
+            }
+        });
+        this.m.endCb = function() {
             _this.btns.next.trigger('click');
             _this.afterEnd();
         }
-        this.m.playStart=function(){
+        this.m.playStart = function() {
             if (_this.m.playStat) {
                 _this.afterStart();
             }
         }
-        this.m.playing(function(){
-            $progress[0].style.width=_this.m.currentTime/_this.m.allTime*100+'%';
-            $nowTime.text(parseInt(_this.m.currentTime/60)+':'+parseInt(_this.m.currentTime%60));
-            $allTime.text(parseInt(_this.m.allTime/60)+':'+parseInt(_this.m.allTime%60));
+        this.m.playing(function() {
+            $progress[0].style.width = _this.m.currentTime / _this.m.allTime * 100 + '%';
+            $nowTime.text(parseInt(_this.m.currentTime / 60) + ':' + parseInt(_this.m.currentTime % 60));
+            $allTime.text(parseInt(_this.m.allTime / 60) + ':' + parseInt(_this.m.allTime % 60));
         });
         this.$listItm.on('click', function(e) {
-            _this.listIndex=_this.$listItm.index($(this));
+            _this.listIndex = _this.$listItm.index($(this));
         });
         this.btns.play.on('click', function(e) {
-            if(_this.m.playStat){
+            if (_this.m.playStat) {
                 _this.m.a.pause();
                 _this.afterEnd();
-            }else{
+            } else {
                 _this.m.a.play();
                 _this.afterStart();
             }
         });
         this.btns.next.on('click', function(event) {
-            if (_this.musicIndex<_this.musicList.length-1) {
+            if (_this.musicIndex < _this.musicList.length - 1) {
                 //_this.musicIndex++;
-                _this.listIndex=_this.musicIndex+1;
-                
+                _this.listIndex = _this.musicIndex + 1;
+
+            } else {
+                _this.musicIndex = 0;
+                _this.listIndex = _this.musicIndex;
             }
 
         });
         this.btns.prev.on('click', function(event) {
-            if (_this.musicIndex>0) {
+            if (_this.musicIndex > 0) {
                 //_this.musicIndex--;
-                _this.listIndex=_this.musicIndex-1;
+                _this.listIndex = _this.musicIndex - 1;
             }
 
         });
     }
 }
-new app({play:$play,prev:$prev,next:$next},media);
+new app({ play: $play, prev: $prev, next: $next }, media);
